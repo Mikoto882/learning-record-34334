78b2a48c0988c64522506361dae06592d418898c
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-06-07 14:20:04 +0900

    バリデーションの設定

--- app/models/learn.rb
+++ app/models/learn.rb
@@ -1,4 +1,7 @@
 class Learn < ApplicationRecord
+  validates :title, presence: true
+  validates :subject_id, numericality: { other_than: 1 , message: "can't be blank" }
+
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :subject
