f56d424203bfa3653327f45459bd9bdf7d3e79c3
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-06-19 02:09:01 +0900

    リンクの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @user = User.find(params[:id])
     @learns = user.learns.order("created_at DESC")
+    @profile = Profile.find(params[:id])
   end
 
 end
