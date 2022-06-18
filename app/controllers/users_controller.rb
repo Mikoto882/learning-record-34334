95270ad3231b636bc4616bc2e4e3bcbb9f188f01
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-06-19 02:58:24 +0900

    usersコントローラーのアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
     @profile = Profile.find(params[:id])
   end
 
+  def edit
+    
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
+  end
+
 end
