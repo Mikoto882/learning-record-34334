2e8cc8f7d0c6959e5fa1a74876173d8ab50bf88d
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-06-19 03:12:48 +0900

    マイグレーションの修正

--- db/migrate/20220605035600_devise_create_users.rb
+++ db/migrate/20220605035600_devise_create_users.rb
@@ -7,6 +7,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.text :introduction
+      t.integer :grade_id
 
       ## Recoverable
       t.string   :reset_password_token
