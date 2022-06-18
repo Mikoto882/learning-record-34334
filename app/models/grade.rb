844d82596603ee0a7704b62fb8f79c2ead930e9e
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-06-18 23:09:10 +0900

    学年選択のモデル作成

---
+++ app/models/grade.rb
@@ -0,0 +1,9 @@
+class Grade < ActiveHash::Base
+  self.date = [
+    { id: 1, name: '学年を選択' },
+    { id: 2, name: '高校1年生' },
+    { id: 3, name: '高校2年生' },
+    { id: 4, name: '高校3年生' },
+    { id: 5, name: 'その他' },
+  ]
+end
