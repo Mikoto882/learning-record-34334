5ab76243ecfe8e35c8e3e8341d322c6b1c3bbb25
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-06-18 23:18:14 +0900

    学年選択のバリデーションの設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ApplicationRecord
+  validates :grade_id, numericality: { other_than: 1, message: "can't be blank" }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :grade
