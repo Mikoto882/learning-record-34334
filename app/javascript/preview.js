dcbb5afed8343104a992355260e82858223462ff
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-07-16 13:41:22 +0900

    値が変化した際の関数の定義

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -6,4 +6,11 @@ document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページフォームがないならここで終了
   if (!learnForm) return null;
   console.log("preview.jsが読み込まれました");
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="learn[image]"]');
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    console.log("input要素で値の変化が起きました");
+  });
 });
