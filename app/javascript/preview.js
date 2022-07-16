6628a1286aaef9d00908a521c91ed95dfec8590f
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-07-16 13:15:04 +0900

    preview.jsの読み込み

---
+++ app/javascript/preview.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // 新規投稿・編集ページのフォームを取得
+  const learnForm = document.getElementById('new_learn');
+  // 新規投稿・編集ページフォームがないならここで終了
+  if (!learnForm) return null;
+  console.log("preview.jsが読み込まれました");
+});
