86309ce2d49ca8022654370ea4fa664c3f44a992
Mikoto882 <hisashi.doraguniru@gmail.com>
2022-07-16 13:21:37 +0900

    プレビュー画像の表示スペース取得

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
   const learnForm = document.getElementById('new_learn');
+  // プレビューを表示するためのスペースを取得
+  const previewList = document.getElementById('previews');
   // 新規投稿・編集ページフォームがないならここで終了
   if (!learnForm) return null;
   console.log("preview.jsが読み込まれました");
